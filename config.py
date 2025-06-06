fa9664e10b44ddf07c506fc29d26f807bf0764a9
HienXuanPham <phamxuanhien02@gmail.com>
2025-06-05 21:42:31 -0400

    config

--- config.py
+++ config.py
@@ -5,8 +5,12 @@ load_dotenv()
 
 class Config:
     MONGODB_URL = os.getenv("MONGODB_URL")
+    MONGODB_DATABASE = os.getenv("MONGODB_DATABASE")
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+    GOOGLE_CLOUD_PROJECT = os.getenv("GOOGLE_CLOUD_PROJECT")
     
     # Collections
     SHELTERS_COLLECTION = "shelters"
     EARTHQUAKES_COLLECTION = "earthquakes"
+    DISASTERS_COLLECTION = "disasters"
+    VECTOR_INDEX_NAME = "shelter_vector_index"
